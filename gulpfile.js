cc5632e96695fc19527c82dc6c1cc271acd8fa58
Transoman <vasiag10@gmail.com>
2017-04-10 22:32:37 +0300

    Оновив файл. Додав нові таски

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,27 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+browserSync = require('browser-sync').create();;
 
-gulp.task("styles", function() {
-	
-}); 
+// Static server
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		},
+		notify: false
+	});
+});
+
+
+gulp.task('styles', function() {
+	return gulp.src('sass/style.sass')
+	.pipe(sass())
+	.pipe(gulp.dest('css'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+	gulp.watch('sass/style.sass'. ['styles']);
+	gulp.watch('*.html').on("change", browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
